8a084ddf9d46d61b8a07ee150518123d4194ed07
Roberto Chang <robertochangpadilla@gmail.com>
2021-02-05 15:49:32 -0400

    Solucionado problemas de registro y login. (Correo y usuarios en uso, usuario no registrado...)

--- js/forms.js
+++ js/forms.js
@@ -1,9 +1,35 @@
+function checkEmail(){
+    var userLog = document.getElementById('user');
+    var emailLog = document.getElementById('email');
+    var usuario = JSON.parse(window.localStorage.getItem(userLog.value));
+
+    for (x=0; x<=localStorage.length-1; x++)  {  
+        clave = localStorage.key(x);
+        var database = JSON.parse(window.localStorage.getItem(clave));
+        if(userLog.value == clave){
+            userLog.style.borderColor = "#ff7c71";
+            return false;
+        }else{
+            userLog.style.borderColor = "#2dbc75";
+        } 
+        
+        if(emailLog.value == database.email){
+            emailLog.style.borderColor = "#ff7c71";
+            return false;
+        }else{
+            emailLog.style.borderColor = "#2dbc75";
+        }
+    }
+    return true;
+}
+
 function check(){
     var password = document.getElementById('pass');
     var password2 = document.getElementById('pass-repeat');
-    
+   
     if(password.value != password2.value){
         password2.style.borderColor = "#ff7c71";
+        alert("Las contraseñas no coinciden");
         return false;
     }else{
         password2.style.borderColor = "#2dbc75";
@@ -26,12 +52,11 @@ function registrar(){
 }
 
 function validar(){
-    if(check()){
+    if(check() && checkEmail()){
         registrar();
         location.href = "../login.html";
         return true;
-    }else{
-        alert("Las contraseñas no coinciden");
+    }else{ 
         return false;
     }
 }
@@ -40,15 +65,31 @@ function Login(){
     var password = document.getElementById('passLog');
     var userLog = document.getElementById('userLog');
     var usuario = JSON.parse(window.localStorage.getItem(userLog.value));
-    console.log(usuario.user);
-    
-    if((usuario.user != userLog.value) || (usuario.pass != password.value)){
+
+    if(userLog.value == "") {
+        alert("Ingresa un usuario.");
+    }else if(password.value == ""){
+        alert("Ingresa una contraseña.");
+    }else if(usuario == null){
+        alert("El usuario no está registrado.");
+        return false;
+    }else if((usuario.user != userLog.value) || (usuario.pass != password.value)){
         password.style.borderColor = "#ff7c71";
-        userLog.style.borderColor = "#ff7c71";
+        alert("Los datos no coinciden.");
         return false;
     }else{
-        alert("Bienvenido "+userLog.value);
+        password.style.borderColor = "#afafaf";
+        userLog.style.borderColor = "#afafaf";
         return true;
     }
     
+}
+
+function logIn(){
+    if(Login()){
+        alert("Bienvenido "+userLog.value);
+        return true;
+    }else{
+        return false;
+    }
 }
